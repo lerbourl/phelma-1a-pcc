083497b3136a2e1393322de733902e3f8d2a313c
colinEP <epalle.c@hotmail.fr>
2018-04-23 00:05:01 +0200

    colin test

--- main.c
+++ main.c
@@ -30,7 +30,9 @@ int main(int argc, char* argv[]){
     //affiche_graphe(g1);
     libere_graphe(g);
     //libere_graphe(g1);
+	int patate = 42;
     libere_sommet(s);
-
+	
+	printf("coucou");
     return EXIT_SUCCESS;
 }
