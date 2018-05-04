fe7a029515cfd802029a44dd2ca39a922ad23cd2
lerbourl <lerbourg.louis@gmail.com>
2018-05-04 14:00:20 +0200

    rectifiation hash

--- hash.h
+++ hash.h
@@ -8,20 +8,4 @@ typedef struct {
     char* nom;
     int index;} H_SOM;
 
-//définir ailleur ce qu'est ELEMENT!
-struct celluleh {
-	H_SOM val;
-	struct celluleh* suiv;};
-typedef struct celluleh* Listeh;
-
-Listeh creer_Listeh();
-int Liste_videh(Listeh l);
-Listeh ajout_teteh(H_SOM e, Listeh l);
-Listeh supprimer_teteh(Listeh l) ;
-void libererh(Listeh l);
-Listeh ajout_queueh(H_SOM e, Listeh l);
-Listeh concathListeh(Listeh l1, Listeh l2);
-Listeh copieh(Listeh l);
-Listeh supprimenh(int n, Listeh l);
-
 #endif
