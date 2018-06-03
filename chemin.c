00768262419738ee7ae46d4de29bfa1388efc892
ColinEP <epallec@hotmail.fr>
2018-06-03 22:10:06 +0200

    clean du code de pcc

--- chemin.c
+++ chemin.c
@@ -33,13 +33,9 @@ void corresp_set_zero(GRAPHE g, H_TABLE ht, H_SOMMET hs) {
 		if (!strcmp(hs_getn(lgetval(l_hs)), hs_getn(hs) ) ) {
 			// parcours de la liste des voisins pour chaque sommet de la correspondance
 			for (l_voisins=g.tab_s[hs_geti(lgetval(l_hs))].voisins ; !liste_vide(l_voisins) ; l_voisins=lgetsuiv(l_voisins)) {
-//				printf("DEBUG CORRESP  ALL i: %d (%s) --> i: %d (%s)\nCOUT : %lf  a: %d\n", hs_geti(lgetval(l_hs)) , g.tab_s[hs_geti(lgetval(l_hs))].nom_noeud , arc_geta(lgetval(l_voisins)) , g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud , arc_getc(lgetval(l_voisins)) , arc_geta(lgetval(l_voisins)) );
 				// si voisin est dans la m^eme station => cout de l'arc mis à 0
 				if (!strcmp( g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud, g.tab_s[hs_geti(hs)].nom_noeud ) ) {  // utilisation de g.tab_s[hs_geti(hs)].nom_noeud plutot que (hs_getn(hs) pour régler les problèmes de MAJ/min
-					// IL FAUT TEST COUT == 360 POUR ETRE SUR QUE ÇA MARCH
-//					printf("       ==>   DEBUG CORRESP SET0 i: %d (%s) --> i: %d (%s)\n       COUT : %lf  a: %d\n", hs_geti(lgetval(l_hs)) , g.tab_s[hs_geti(lgetval(l_hs))].nom_noeud , arc_geta(lgetval(l_voisins)) , g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud , arc_getc(lgetval(l_voisins)) , arc_geta(lgetval(l_voisins)) );
 					arc_setc(lgetval(l_voisins), 0);
-//					printf("       ==>   DEBUG CORRESP SET0 i: %d (%s) --> i: %d (%s)\n       COUT : %lf  a: %d\n", hs_geti(lgetval(l_hs)) , g.tab_s[hs_geti(lgetval(l_hs))].nom_noeud , arc_geta(lgetval(l_voisins)) , g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud , arc_getc(lgetval(l_voisins)) , arc_geta(lgetval(l_voisins)) );
 				}
 			}
 		}
@@ -58,13 +54,9 @@ void corresp_set_360(GRAPHE g, H_TABLE ht, H_SOMMET hs) {
 		if (!strcmp(hs_getn(lgetval(l_hs)), hs_getn(hs) ) ) {
 			// parcours de la liste des voisins pour chaque sommet de la correspondance
 			for (l_voisins=g.tab_s[hs_geti(lgetval(l_hs))].voisins ; !liste_vide(l_voisins) ; l_voisins=lgetsuiv(l_voisins)) {
-//				printf("DEBUG CORRESP  ALL i: %d (%s) --> i: %d (%s)\nCOUT : %lf  a: %d\n", hs_geti(lgetval(l_hs)) , g.tab_s[hs_geti(lgetval(l_hs))].nom_noeud , arc_geta(lgetval(l_voisins)) , g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud , arc_getc(lgetval(l_voisins)) , arc_geta(lgetval(l_voisins)) );
 				// si voisin est dans la m^eme station => cout de l'arc mis à 0
 				if (!strcmp( g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud, g.tab_s[hs_geti(hs)].nom_noeud ) ) {  // utilisation de g.tab_s[hs_geti(hs)].nom_noeud plutot que (hs_getn(hs) pour régler les problèmes de MAJ/min
-					// IL FAUT TEST COUT == 360 POUR ETRE SUR QUE ÇA MARCH
-//					printf("       ==>   DEBUG CORRESP SET0 i: %d (%s) --> i: %d (%s)\n       COUT : %lf  a: %d\n", hs_geti(lgetval(l_hs)) , g.tab_s[hs_geti(lgetval(l_hs))].nom_noeud , arc_geta(lgetval(l_voisins)) , g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud , arc_getc(lgetval(l_voisins)) , arc_geta(lgetval(l_voisins)) );
 					arc_setc(lgetval(l_voisins), 360);
-//					printf("       ==>   DEBUG CORRESP SET0 i: %d (%s) --> i: %d (%s)\n       COUT : %lf  a: %d\n", hs_geti(lgetval(l_hs)) , g.tab_s[hs_geti(lgetval(l_hs))].nom_noeud , arc_geta(lgetval(l_voisins)) , g.tab_s[arc_geta(lgetval(l_voisins))].nom_noeud , arc_getc(lgetval(l_voisins)) , arc_geta(lgetval(l_voisins)) );
 				}
 			}
 		}
@@ -98,21 +90,21 @@ L_ARC chemin_supr_zero(L_ARC chemin, int* pd, int* pa){
 
 /*algorithme de plus court chemin avec gestion par tas*/
 L_ARC pcc(GRAPHE g, int d, int a) {
-    if(d == a){
+	/* Cas si a == b */
+	if(d == a){
         printf("\n Tu y es déjà, petit malin !!!");
         return NULL;
     }
-	int i;
+    
 	/* INIT */
+	int i;
+	
 	double* tab_pcc = malloc(g.nb_s * sizeof(double));
 	err_ctrl(tab_pcc, "erreur tableau dynamique tab_pcc", __FILE__, __func__, __LINE__, "");
 
 	int* pere = malloc(g.nb_s * sizeof(int));
 	err_ctrl(pere, "erreur tableau dynamique pere", __FILE__, __func__, __LINE__, "");
 
-	/* s_atteint = tableau des sommets atteint :
-	   if s_atteint[j]==0 : j dans C (ensemble des sommets qui restent a visiter)
-	   if s_atteint[j]==1 : j dans S (ensemble des sommets dont on connait le ppc en partant de d) */
 	int* tas_C = calloc(g.nb_s, sizeof(int));
 	err_ctrl(tas_C, "erreur tableau dynamique tas_C", __FILE__, __func__, __LINE__, "");
 
@@ -136,15 +128,15 @@ L_ARC pcc(GRAPHE g, int d, int a) {
 	/* Algo */
 	int j;
 	int k;
-
 	L_ARC v;
 
 	do {
 		/* recherche du sommet j dans C avec la val de tab_pcc la plus petite */
 		j = tas_C[0];   // la min est à la racine du tas
-
-/**/	if (tab_pcc[j] == INFINITY) {		// pas de min != INF non atteint trouvé			///////////////////////////////////////////////////////////
-											// cas pas de solution    equivalent au test pcc[j]!= +inf de l'énoncé   ///////////////////////////////////////////////////////////
+		
+		/* Test cas pas de solution */
+		if (tab_pcc[j] == INFINITY) {		// pas de min != INF non atteint trouvé
+											// cas pas de solution : equivalent au test pcc[j]!= +inf de l'énoncé   
 			printf("\nPas de chemin possible, les voitures ne roulent pas sur l'eau (ou à contre-sens)\n");
 			/* Libération */
 			free(tab_pcc);
@@ -153,31 +145,17 @@ L_ARC pcc(GRAPHE g, int d, int a) {
 			free(pcc_to_tas);
 			return NULL;
 		}
-		suppressiontas(tas_C, tab_pcc, pcc_to_tas, len_tas);   //j devient atteint
-		len_tas--;    // L'ensemble C perd 1 élémént
-/*
-printf("DEBUG tas\n");
-for(i=0;i<g.nb_s;i++)  {
-printf("tas  %d  %d  %lf     %d\n", i,tas_C[i], tab_pcc[tas_C[i]], pcc_to_tas[tas_C[i]]);
-}*/
+		suppressiontas(tas_C, tab_pcc, pcc_to_tas, len_tas);	//j devient atteint
+		len_tas--;												// L'ensemble C perd 1 élémént
+
 		/* maj du pcc pour tous les voisins de j */
 		for(v=g.tab_s[j].voisins ; !liste_vide(v) ; v=lgetsuiv(v)) {
 			k = arc_geta((T_ARC)lgetval(v));
 			if (tab_pcc[k] > tab_pcc[j] + arc_getc((T_ARC)lgetval(v))) {
 				tab_pcc[k] = tab_pcc[j] + arc_getc((T_ARC)lgetval(v));
-/*
-printf("DEBUG tas0\n");
-for(i=0;i<g.nb_s;i++)  {
-printf("tas0 %d  %d  %lf     %d\n", i,tas_C[i], tab_pcc[tas_C[i]], pcc_to_tas[tas_C[i]]);
-}*/
 
 				// maj tas   il faut:  index k dans pcc => index dans tas
 				augmentetas(tas_C, tab_pcc, pcc_to_tas, pcc_to_tas[k]);
-/*
-printf("DEBUG tasX\n");
-for(i=0;i<g.nb_s;i++)  {
-printf("tasX %d  %d  %lf     %d\n", i,tas_C[i], tab_pcc[tas_C[i]], pcc_to_tas[tas_C[i]]);
-}*/
 
 				pere[arc_geta((T_ARC)lgetval(v))] = j;
 			}
